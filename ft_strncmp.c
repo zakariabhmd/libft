7a897e96ea64667f0ebe27429753990fbc344786
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2022-10-10 04:00:36 +0100

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: zbabahmi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 01:24:42 by zbabahmi          #+#    #+#             */
-/*   Updated: 2022/10/07 01:24:44 by zbabahmi         ###   ########.fr       */
+/*   Updated: 2022/10/10 03:58:16 by zbabahmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,24 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	size_t			i;
+	unsigned char	*z1;
+	unsigned char	*z2;
 
+	z1 = (unsigned char *)s1;
+	z2 = (unsigned char *)s2;
 	i = 0;
-	while (s1[i] && i < n)
+	while (z1[i] != '\0' && i < n)
 	{
-		if (s1[i] != s2[i])
+		if (z1[i] != z2[i])
 		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+			return (z1[i] - z2[i]);
 		}
 		i++;
 	}
-	if(s2[i] == '\0' || i == 0)
-		return (0);
-	return (-s2[i]);
+	if (z1[i] == '\0' && i < n)
+	{
+		return (z1[i] - z2[i]);
+	}
+	return (0);
 }
